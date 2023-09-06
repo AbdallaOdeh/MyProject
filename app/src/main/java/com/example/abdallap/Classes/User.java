f1c6b301b1f685ed454f1bac627d57344d503764
AbdallaOdeh <abdallaodeh604@gmail.ocm>
2023-09-06 14:23:26 +0300

    first step

---
+++ app/src/main/java/com/example/abdallap/Classes/User.java
@@ -0,0 +1,12 @@
+package com.example.abdallap.Classes;
+
+public class User {
+    private String email;
+    private String password;
+    public User(String email,String pass){
+        this.email=email;
+        this.password=pass;
+
+    }
+}
+
