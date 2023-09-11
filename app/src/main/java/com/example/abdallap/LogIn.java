9c7d70f0913d269760e56f5cea05cfab38fa21a7
AbdallaOdeh <abdallaodeh604@gmail.ocm>
2023-09-11 09:56:25 +0300

    Login page and signupPage

---
+++ app/src/main/java/com/example/abdallap/LogIn.java
@@ -0,0 +1,67 @@
+package com.example.abdallap;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LogIn extends AppCompatActivity {
+    EditText emailEditText;
+    EditText passwordEditText;
+    Button loginButton;
+    TextView newAccountButton;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        emailEditText=findViewById(R.id.ptEmail);
+        passwordEditText=findViewById(R.id.ptPassword);
+        loginButton=findViewById(R.id.btLogIn2);
+        newAccountButton=findViewById(R.id.tvSignUp);
+        newAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LogIn.this,SignUp.class));
+            }
+        });
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final FirebaseAuth mAuth=FirebaseAuth.getInstance();
+                mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                        .addOnCompleteListener(LogIn.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                    startActivity(new Intent(LogIn.this,MainActivity.class));
+
+                                } else {
+
+                                    Log.d("error","error");
+
+                                }
+                            }
+                        });
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/abdallap/MainActivity.java
+++ app/src/main/java/com/example/abdallap/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.abdallap;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Button LogIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LogIn=findViewById(R.id.btLogIn);
+        LogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LogIn.class));
+
+            }
+        });
+
     }
 }
