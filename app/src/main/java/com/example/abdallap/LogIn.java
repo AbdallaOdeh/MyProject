8fbca0deeaecc3c5f10f264af3a9a36cdc75d982
Fl4ck <abdallaodeh605@gmail.com>
2023-09-17 20:51:04 +0300

    homework

--- app/src/main/java/com/example/abdallap/LogIn.java
+++ app/src/main/java/com/example/abdallap/LogIn.java
@@ -17,7 +17,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LogIn extends AppCompatActivity {
+public class LogIn extends AppCompatActivity {  
     EditText emailEditText;
     EditText passwordEditText;
     Button loginButton;
