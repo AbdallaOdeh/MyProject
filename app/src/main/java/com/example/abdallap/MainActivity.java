0d64ba5b0a462384860afa5028ecc634622bc21c
AbdallaOdeh <abdallaodeh604@gmail.ocm>
2023-09-19 11:10:11 +0300

    classwork

--- app/src/main/java/com/example/abdallap/MainActivity.java
+++ app/src/main/java/com/example/abdallap/MainActivity.java
@@ -10,22 +10,44 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    TextView username,email;
 
     private DrawerLayout drawerLayout;
-
+    private FirebaseAuth fauth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initialize Firebase Auth
+        fauth = FirebaseAuth.getInstance();
+
 
         Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
         setSupportActionBar(toolbar);
-
+        FirebaseUser user = fauth.getCurrentUser();
+        if (user!=null){
+            NavigationView navigationView = findViewById(R.id.nav_view);
+            View headerview = navigationView.getHeaderView(0);
+            username =(TextView) headerview.findViewById(R.id.tvUserName);
+            email = (TextView) headerview.findViewById(R.id.tvUserEmail);
+            username.setText(user.getDisplayName());
+            email.setText(user.getEmail());
+        }else{
+            Intent i= new Intent(MainActivity.this,LogIn.class);
+            startActivity(i);
+        }
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -39,8 +61,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_home);
         }
+
+
     }
 
+
+    @Override
+    protected void onRestart() {
+
+        super.onRestart();
+    }
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+    }
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         if(R.id.nav_home==item.getItemId()){
